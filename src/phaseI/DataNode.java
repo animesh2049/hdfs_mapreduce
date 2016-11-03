f28fa4f3a78452f7b107d92798630e96779e2ec7
ayushi1411 <bknashi@gmail.com>
2016-11-03 23:23:43 +0530

    test

--- src/phaseI/DataNode.java
+++ src/phaseI/DataNode.java
@@ -8,7 +8,18 @@ public class DataNode implements DataNodeRemoteInterfaces {
 	public DataNode(){}
 	
 	public byte[] writeBlock(byte[] message) {
-		
+		Class.forName("com.mysql.jdbc.Driver");
+		try {
+			Connection con=DriverManager.getConnection("jdbc:mysql://localhost:3306","root","ishi2002");
+			Statement stmt=con.createStatement();
+			stmt.executeUpdate("create database if not exists student");
+			stmt.execute("use student");
+			stmt.executeUpdate("create table if not exists personal(rollno int auto_increment,name varchar(20),age int,address varchar(20),primary key(rollno))");
+			stmt.executeUpdate("insert into personal(name,age,address) values('ayushi',18,'jnv')");
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} 
 	}
 	
 	public byte[] readBlock(byte[] message) {
